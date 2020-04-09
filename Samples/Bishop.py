1b99aefed1ae4b596f24cfbf49ef92a3bcc7645d
Haseltalab <53440292+Haseltalab@users.noreply.github.com>
2020-04-09 19:21:31 +0300

    Update Bishop.py

--- Samples/Bishop.py
+++ Samples/Bishop.py
@@ -9,5 +9,5 @@ from LIPRO import *
 
 profile1 = Load('text files/Bishop','profile')
 path = polygon(4,50,90)
-path = HTM3D(np.insert(path,2,0,axis=1),0,90,0,-4,0,0)
+path = Transform(np.insert(path,2,0,axis=1),0,90,0,-4,0,0)
 part1 = sweep(profile1,profile1,path)
