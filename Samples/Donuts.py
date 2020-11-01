a1b0a7a5a8ea0073ce95f749695650c838579e9d
Haseltalab <53440292+Haseltalab@users.noreply.github.com>
2020-11-01 12:11:10 +0300

    Update and rename Example1.py to Donuts.py

--- Samples/Donuts.py
+++ Samples/Donuts.py
@@ -9,14 +9,14 @@ from LIPRO import *
 
 G1 = np.insert(polygon(1,50),2,0,axis=1)
 G1 = Transform(G1,90,0,0,0,0,0)
-S1 = np.array(polygon(1,50)) + [-1,0]
-part1 = sweep(S1,S1,G1)
+P1 = np.array(polygon(1,50)) + [-1,0]
+part1 = sweep(P1,P1,G1)
 part1_ver = Transform(part1[0],0,0,0,0,0,1)
 
 G2 = np.insert(polygon(1,50),2,0,axis=1)
 G2 = Transform(G2,0,90,90,0,0,0)
-S2 = np.array(polygon(1,50,90)) + [0,1]
-part2 = sweep(S2,S2,G2)
+P2 = np.array(polygon(1,50,90)) + [0,1]
+part2 = sweep(P2,P2,G2)
 part2_ver = Transform(part2[0],0,0,0,0,0,3)
 
 #savestl('ring',part1_ver,part1[1])
